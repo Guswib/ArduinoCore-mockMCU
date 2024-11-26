b741b8f96e9de3a481178aa197442cd47a95ea59
Gustav Wiberg <gustav.wiberg@gmail.com>
2024-11-26 16:17:14 +0100

    testing

---
+++ build/CMakeFiles/exmaple_test.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Common.cpp" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Common.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Common.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Print.cpp" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Print.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Print.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Stream.cpp" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Stream.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/Stream.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/String.cpp" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/String.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-API/api/String.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/cores/arduino/dtostrf.cpp" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/cores/arduino/dtostrf.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/cores/arduino/dtostrf.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/cores/arduino/itoa.cpp" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/cores/arduino/itoa.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/C_/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/cores/arduino/itoa.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/MillisFake.cpp" "CMakeFiles/exmaple_test.dir/src/MillisFake.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/MillisFake.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/PrintMock.cpp" "CMakeFiles/exmaple_test.dir/src/PrintMock.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/PrintMock.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_find.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_find.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_find.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_findUntil.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_findUntil.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_findUntil.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_getTimeout.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_getTimeout.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_getTimeout.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_parseFloat.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_parseFloat.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_parseFloat.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_parseInt.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_parseInt.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_parseInt.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_readBytes.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_readBytes.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_readBytes.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_readBytesUntil.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_readBytesUntil.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_readBytesUntil.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_readString.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_readString.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_readString.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_readStringUntil.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_readStringUntil.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_readStringUntil.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/Stream/test_setTimeout.cpp" "CMakeFiles/exmaple_test.dir/src/Stream/test_setTimeout.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/Stream/test_setTimeout.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/StreamMock.cpp" "CMakeFiles/exmaple_test.dir/src/StreamMock.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/StreamMock.cpp.obj.d"
+  "C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test/src/main.cpp" "CMakeFiles/exmaple_test.dir/src/main.cpp.obj" "gcc" "CMakeFiles/exmaple_test.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
