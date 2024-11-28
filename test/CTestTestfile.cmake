0b7f6fb6a28eee738b39d834d98bbb4335c994c2
Gustav Wiberg <52791556+Guswib@users.noreply.github.com>
2024-11-28 13:15:26 +0100

    Delete test/CTestTestfile.cmake

--- test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test
-# Build directory: C:/Users/gustav.wiberg/Documents/GitHub/Arduino/ArduinoCore-mockMCU/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
