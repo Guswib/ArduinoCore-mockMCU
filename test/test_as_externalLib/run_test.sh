094525bbe72989b5576ffb2602b80c643bcd2f6c
Wiberg, Gustav Karl Henrik (DCBP) <gustav.wiberg@gmail.com>
2026-02-28 11:57:24 -0800

    ff

--- test/test_as_externalLib/run_test.sh
+++ test/test_as_externalLib/run_test.sh
@@ -3,9 +3,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Script location: ${SCRIPT_DIR}"
 
-# cmake -G "MinGW Makefiles" -S "$SCRIPT_DIR" -B "$SCRIPT_DIR/build" -D CMAKE_POLICY_VERSION_MINIMUM=3.5 -W no-dev
-cmake -G "MinGW Makefiles" -S "$SCRIPT_DIR" -B "$SCRIPT_DIR/build" -W no-dev
+cmake -G "MinGW Makefiles" -S "$SCRIPT_DIR" -B "$SCRIPT_DIR/build" -D CMAKE_POLICY_VERSION_MINIMUM=3.5 -W no-dev
+# cmake -G "MinGW Makefiles" -S "$SCRIPT_DIR" -B "$SCRIPT_DIR/build" -W no-dev
 
 cmake --build "$SCRIPT_DIR/build"
 
 "$SCRIPT_DIR/build/UnitTest.exe" 
+
+
+cd "$SCRIPT_DIR/build/"
+ctest
