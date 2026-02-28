49d0d4379c6b46c8cee8bfcbd301444f806ba219
Wiberg, Gustav Karl Henrik (DCBP) <gustav.wiberg@gmail.com>
2026-02-28 10:37:29 -0800

    adding bash files

---
+++ test/build_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Script location: ${SCRIPT_DIR}"
+
+cmake -G "MinGW Makefiles" -S "$SCRIPT_DIR" -B "$SCRIPT_DIR/build" -D CMAKE_POLICY_VERSION_MINIMUM=3.5 -W no-dev
+cmake --build "$SCRIPT_DIR/build"
+"$SCRIPT_DIR/build/UnitTest.exe" 
